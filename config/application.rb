92368c9000364492d090872cdd5e74ffc98d0693
Thomas Fitzgerald <tfitzapps@gmail.com>
2017-03-16 19:24:53 +0000

    first changes

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module HelloRails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.log_level = :warn
   end
 end
